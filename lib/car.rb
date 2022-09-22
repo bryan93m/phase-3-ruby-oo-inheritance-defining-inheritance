03155fb78d493971303c29b9cf33c51f0076475b
Bryan Meadows <bryan1993meadows@gmail.com>
2022-09-22 16:25:30 -0600

    commit tree 9950362c11dcaae5ef1736a14993f04f214d4ba9

--- lib/car.rb
+++ lib/car.rb
@@ -1,2 +1,16 @@
 # This loads the code from the vehicle.rb file so we can access its code within this file
 require_relative './vehicle'
+class Car < Vehicle
+    
+    def initialize(wheel_size, wheel_number)
+        super(wheel_size, wheel_number)
+    end
+
+    def go
+        return 'VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!'
+    end
+
+    def fill_up_tank
+       super
+    end
+end
